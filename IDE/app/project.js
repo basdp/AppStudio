fe2f7542ce6f64a4d183c355189cbc10deaf47c9
Bas du Pré <me@basdupre.com>
2014-06-05 15:42:44 +0200

    p

--- IDE/app/project.js
+++ IDE/app/project.js
@@ -1,4 +1,18 @@
+var project = {
+	name: "",
+	location: "",
+	files: [
+		{name: 'test', children: []},
+		{name: 'test2', children: [
+			{name: 'test3', children: []},
+		]},
+		{name: 'test4', children: []},
+	],
+};
+
 function new_project(name, location) {
+	var fs = require('fs');
+
 	name = name.trim();
 	if (name == "") {
 		show_messagebox("Name cannot be empty");
@@ -11,5 +25,93 @@ function new_project(name, location) {
 		return false;	
 	}
 	
+	if (fs.existsSync(location)) {
+		show_messagebox(location + " already exists!");
+		return false;
+	}
+
+	fs.mkdirSync(location);
+	if (!fs.existsSync(location)) {
+		show_messagebox("Could not create directory " + location);
+		return false;
+	}
+
+	project.name = name;
+	project.location = location;
+	project.files = [];
+
+	rebuild_project_treeview();
+
 	return true;
+}
+
+function get_icon_for_filename(filename) {
+	var ext = filename.substr(filename.lastIndexOf('.') + 1);
+	switch (ext.toLowerCase()) {
+		case "js": return 'images/icons/file-js.png';
+		case "xml": return 'images/icons/file-xml.png';
+		case "des": return 'images/icons/file-des.png';
+		case "proj": return 'images/icons/file-proj.png';
+		case "config": return 'images/icons/file-config.png';
+		default: return 'images/icons/file-unknown.png';
+	}
+}
+
+function add_file_to_tree(file, parentUl) {
+	var node = document.createElement('li');
+	var img = document.createElement('img');
+	if (file.children.length > 0) {
+		// this is a folder
+		img.src = 'images/icons/folder.png';
+		node.onrightclick = project_open_folder_contextmenu;
+	} else {
+		img.src = get_icon_for_filename(file.name);
+		node.onrightclick = project_open_file_contextmenu;
+	}
+	node.setAttribute('data-location', file.location);
+	node.appendChild(img);
+	node.appendChild(document.createTextNode(" " + file.name));
+	if (file.children.length > 0) {
+		var ul = document.createElement('ul');
+		node.appendChild(ul);
+		file.children.forEach(function(subfile, i) {
+			add_file_to_tree(subfile, ul);
+		});
+	}
+	parentUl.appendChild(node);
+}
+
+function rebuild_project_treeview() {
+	var projectNode = document.createElement('li');
+	projectNode.onrightclick = project_open_folder_contextmenu;
+	var img = document.createElement('img');
+	img.src = "images/icons/file-project.png";
+	projectNode.appendChild(img);
+	projectNode.appendChild(document.createTextNode(" " + project.name));
+	projectNode.setAttribute('data-location', project.location);
+	var projectUl = document.createElement('ul');
+	projectNode.appendChild(projectUl);
+	project.files.forEach(function(file, i) {
+		add_file_to_tree(file, projectUl);
+	});
+	var ul = document.querySelector(".projecttree .treeview");
+	ul.innerHTML = "";
+	ul.appendChild(projectNode);
+
+	activate_treeview(ul);
+}
+
+var projectFolderMenu = new gui.Menu();
+projectFolderMenu.append(new gui.MenuItem({ label: 'Item A' }));
+projectFolderMenu.append(new gui.MenuItem({ label: 'Item B' }));
+projectFolderMenu.append(new gui.MenuItem({ type: 'separator' }));
+projectFolderMenu.append(new gui.MenuItem({ label: 'Item C' }));
+function project_open_folder_contextmenu(e) {
+	projectFolderMenu.popup(e.pageX, e.pageY);
+}
+
+var projectFileMenu = new gui.Menu();
+projectFileMenu.append(new gui.MenuItem({ label: 'Item A' }));
+function project_open_file_contextmenu(e) {
+	projectFileMenu.popup(e.pageX, e.pageY);
 }
--- IDE/app/treeview.js
+++ IDE/app/treeview.js
@@ -1,48 +1,62 @@
 window.addEventListener('DOMContentLoaded', function(){
     var treeviews = document.querySelectorAll(".treeview");
     Array.prototype.forEach.call(treeviews, function(el, i) {
-        // expandable nodes
-        var subtrees = el.querySelectorAll("li > ul");
-        Array.prototype.forEach.call(subtrees, function(el, i) {
-            var expandableNode = el.parentElement;
-            expandableNode.addEventListener('dblclick', function(e) {
-                if (e.y < expandableNode.getBoundingClientRect().top + 20) {
-                    if (expandableNode.classList.contains("closed")) {
-                        // open the node
-                        expandableNode.classList.remove("closed");
-                    } else {
-                        expandableNode.classList.add("closed");
-                    }
-                }
-                e.stopPropagation();
-            });
-            expandableNode.addEventListener('mouseup', function(e) {
-                if (e.x < expandableNode.getBoundingClientRect().left) {
-                    if (expandableNode.classList.contains("closed")) {
-                        // open the node
-                        expandableNode.classList.remove("closed");
-                    } else {
-                        expandableNode.classList.add("closed");
-                    }
-                }
-                e.stopPropagation();
-            });
-        });
-        
-        // node selection
-        var allitems = el.querySelectorAll("li");
-        Array.prototype.forEach.call(allitems, function(el, i) {
-            el.addEventListener('mousedown', function(e) {
-                if (e.y > el.getBoundingClientRect().top + 20 || e.x < el.getBoundingClientRect().left) {
-                    return false;
-                }
-                
-                Array.prototype.forEach.call(allitems, function(el, i) {
-                    el.classList.remove('selected');
-                });
-                el.classList.add('selected');
-                e.stopPropagation();
-            });
-        });
+        activate_treeview(el);
     });
 });
+
+function activate_treeview(el) {
+	// expandable nodes
+	var subtrees = el.querySelectorAll("li > ul");
+	Array.prototype.forEach.call(subtrees, function(el, i) {
+		var expandableNode = el.parentElement;
+		expandableNode.addEventListener('dblclick', function(e) {
+			if (e.y < expandableNode.getBoundingClientRect().top + 20) {
+				if (expandableNode.classList.contains("closed")) {
+					// open the node
+					expandableNode.classList.remove("closed");
+				} else {
+					expandableNode.classList.add("closed");
+				}
+			}
+			e.stopPropagation();
+		});
+		expandableNode.addEventListener('mouseup', function(e) {
+			if (e.x < expandableNode.getBoundingClientRect().left) {
+				if (expandableNode.classList.contains("closed")) {
+					// open the node
+					expandableNode.classList.remove("closed");
+				} else {
+					expandableNode.classList.add("closed");
+				}
+			}
+			e.stopPropagation();
+		});
+	});
+
+	// node selection
+	var allitems = el.querySelectorAll("li");
+	Array.prototype.forEach.call(allitems, function(el, i) {
+		el.addEventListener('mousedown', function(e) {
+			if (e.y > el.getBoundingClientRect().top + 20 || e.x < el.getBoundingClientRect().left) {
+				return false;
+			}
+
+			Array.prototype.forEach.call(allitems, function(el, i) {
+				el.classList.remove('selected');
+			});
+			el.classList.add('selected');
+
+			if (e.button === 2) {
+				// right click
+				if (el.onrightclick !== undefined) {
+					setTimeout(function() {
+						el.onrightclick(e);
+					}, 10);
+				}
+			}
+
+			e.stopPropagation();
+		});
+	});
+}
